3d718096ceef1c1aba4f5b627d35b73c19ecdcea
AgesaEugene <eugenethebandit@gmail.com>
2024-09-09 05:09:19 +0300

    html templates created

--- website/views.py
+++ website/views.py
@@ -4,5 +4,14 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
+
+@views.route('/add')
+def add():
+    return render_template("add.html", user=current_user)
+
+@views.route('/view')
+def view():
+    return render_template("view.html", user=current_user)
